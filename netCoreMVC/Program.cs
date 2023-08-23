3becc1fb6ea81775c1f0d24e9e65d11f2bd2178f
Trần Thanh Nhân <90546715+nhanne@users.noreply.github.com>
2023-08-23 14:11:23 +0700

    update Initalize database .Net MVC EF Core

--- netCoreMVC/Program.cs
+++ netCoreMVC/Program.cs
@@ -1,9 +1,33 @@
+using netCoreMVC.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. buo 1
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SchoolContext>(options =>
+               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<SchoolContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,8 +44,10 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// buoc 2
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
