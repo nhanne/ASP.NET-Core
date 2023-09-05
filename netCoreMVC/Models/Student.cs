10c9821f03bfd10c2224b8c5c559d68501dce17f
Trần Thanh Nhân <90546715+nhanne@users.noreply.github.com>
2023-09-05 17:47:54 +0700

    something

--- netCoreMVC/Models/Student.cs
+++ netCoreMVC/Models/Student.cs
@@ -1,11 +1,26 @@
-﻿namespace netCoreMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace netCoreMVC.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
